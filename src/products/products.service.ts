c9df7290bf1c00774c2e0ea0911eb3e385c3ca22
JFigueroatz <devfigueroatz@gmail.com>
2025-09-17 14:58:07 -0600

    Frist commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('products-service');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected database');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(paginaationDto: PaginationDto) {
+
+    const { page = 1, limit = 10 } = paginaationDto;
+
+    const totalPages = await this.product.count({ where: { available: true } });
+
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: { available: true },
+      }),
+      meta:{
+        total: totalPages,
+        page: page,
+        lastPage: lastPage,
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({ where: { id, available: true } });
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    const {id: __, ... data} = updateProductDto;
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+
+    return product;
+
+  }
+}
